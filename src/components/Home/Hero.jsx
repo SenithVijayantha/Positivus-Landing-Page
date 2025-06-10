c43c86c43ca87933b8e65be220284c46dff46037
M.W.S.S. Vijayantha <ssvsenith@gmail.com>
2025-06-10 22:35:02 +0530

    feat: Change the text sizes for larger screens.

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -5,8 +5,8 @@ import CompanyLogoRow from "./CompanyLogoRow";
 
 const Hero = () => {
   return (
-    <>
-      <div className="lg:hidden flex flex-col items-start sm:items-center">
+    <div className="flex flex-col justify-center items-center">
+      <div className="lg:hidden flex flex-col items-start sm:items-center px-4 py-4 sm:px-12">
         <h1 className="leading-[3.5rem] sm:mb-4 sm:text-center sm:text-7xl text-5xl font-bold">
           Navigating the <br /> digital landscape <br /> for success
         </h1>
@@ -24,14 +24,14 @@ const Hero = () => {
           <CTABtn></CTABtn>
         </div>
       </div>
-      <div className="hidden lg:flex flex-row items-center justify-between">
+      <div className="hidden lg:flex flex-row items-center justify-between px-4 py-4 sm:px-12 xl:w-full">
         <div className="flex flex-col items-start">
-          <h1 className="leading-[3.5rem] sm:mb-4 text-5xl font-bold text-primary-color">
+          <h1 className="leading-[3.5rem] sm:mb-4 text-5xl xl:text-8xl font-bold text-primary-color">
             Navigating the <br /> digital landscape <br /> for success
           </h1>
 
           <div className="sm:max-w-md">
-            <p className="text-lg sm:text-xl lg:text-base">
+            <p className="text-lg sm:text-xl lg:text-base xl:text-2xl">
               Our digital marketing agency helps business grow and succeed
               online through a range of services including SEO, PPC, social
               media marketing and content creation.
@@ -49,7 +49,7 @@ const Hero = () => {
         </div>
       </div>
       <CompanyLogoRow/>
-    </>
+    </div>
   );
 };
 
