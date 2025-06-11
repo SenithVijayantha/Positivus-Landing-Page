45080cdc8b25519f3d9ec4723f3a766856d63fae
M.W.S.S. Vijayantha <ssvsenith@gmail.com>
2025-06-11 15:55:00 +0530

    feat: Add a animation to CompanyLogoRow component to make it scroll infinitely

--- src/components/Home/CompanyLogoRow.jsx
+++ src/components/Home/CompanyLogoRow.jsx
@@ -3,14 +3,21 @@ import { amazon, dribbble, hubspot, netflix, notion, zoom } from "../../assets";
 
 const CompanyLogoRow = () => {
   return (
-    <div className="scroller sm:max-w-sm lg:max-w-5xl mx-auto ">
-      <div className="scroller__inner flex flex-wrap justify-center gap-4 sm:gap-8 my-8 lg:justify-between">
-        <img src={amazon} alt="" className="w-24 object-contain" />
-        <img src={dribbble} alt="" className="w-24 object-contain" />
-        <img src={hubspot} alt="" className="w-24 object-contain" />
-        <img src={notion} alt="" className="w-24 object-contain" />
-        <img src={netflix} alt="" className="w-24 object-contain" />
-        <img src={zoom} alt="" className="w-24 object-contain" />
+    <div className="overflow-hidden w-full bg-transparent py-4">
+      <div className="flex animate-scroll-x whitespace-nowrap items-center gap-8">
+        <img src={amazon} alt="Amazon" className="w-24 object-contain inline-block" />
+        <img src={dribbble} alt="Dribbble" className="w-24 object-contain inline-block" />
+        <img src={hubspot} alt="Hubspot" className="w-24 object-contain inline-block" />
+        <img src={notion} alt="Notion" className="w-24 object-contain inline-block" />
+        <img src={netflix} alt="Netflix" className="w-24 object-contain inline-block" />
+        <img src={zoom} alt="Zoom" className="w-24 object-contain inline-block" />
+        {/* Duplicate for infinite effect */}
+        <img src={amazon} alt="Amazon" className="w-24 object-contain inline-block" />
+        <img src={dribbble} alt="Dribbble" className="w-24 object-contain inline-block" />
+        <img src={hubspot} alt="Hubspot" className="w-24 object-contain inline-block" />
+        <img src={notion} alt="Notion" className="w-24 object-contain inline-block" />
+        <img src={netflix} alt="Netflix" className="w-24 object-contain inline-block" />
+        <img src={zoom} alt="Zoom" className="w-24 object-contain inline-block" />
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,17 @@ export default {
     fontFamily: {
       
     },
-    extend: {},
+    extend: {
+      animation: {
+        'scroll-x': 'scroll-x 10s linear infinite',
+      },
+      keyframes: {
+        'scroll-x': {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+      },
+    },
   },
   plugins: [],
 }
